19bbb0aca18e3ebe72ea11972e0cc4d36431aeed
Rüstem Pek <66913935+rpek99@users.noreply.github.com>
2022-03-17 00:17:23 +0300

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
     networks: {
         ropsten: {
             url: 'https://eth-ropsten.alchemyapi.io/v2/90Vh2Mq1Bu5JSM4rLFgdvEo4Vtev5Nue',
-            accounts: [ 'ef81326770ed3a8a08e1369d3cae559762fe786550955e6137ef7aeb81592014' ]
+            accounts: [ 'privateKey' ]
         }
     }
 }
